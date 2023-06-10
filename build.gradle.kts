974c800cfa91ff83e7eee514b6199ad94eaddfb4
NFRCG <nolanminecraftserver@gmail.com>
2023-06-10 16:42:49 -0500

    chore: convert to gradle

---
+++ build.gradle.kts
@@ -0,0 +1,110 @@
+group = "org.black_ixx"
+version = "2.1.0"
+description = "BossShopPro"
+
+plugins {
+    id("java-library")
+    id("xyz.jpenilla.run-paper") version "2.1.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+repositories {
+    mavenCentral()
+    maven("https://papermc.io/repo/repository/maven-public/") //Paper
+    maven("https://repo.glaremasters.me/repository/public/") //TODO: find way to remove
+    maven("https://repo.worldguard.com.au/repository/maven-public") //TODO: find way to remove
+    maven("https://repo.extendedclip.com/content/repositories/placeholderapi/") //PlaceholderAPI
+    maven("https://jitpack.io") //Vault, Jobs
+    maven("https://ci.lilypadmc.org/plugin/repository/project/Client-Connect-API/Build/19/repository/") //LilyPad
+    maven("https://nexus.bencodez.com/repository/maven-public/") //VotingPlugin
+    maven("https://raw.githubusercontent.com/TeamVK/maven-repository/master/release/") //TokenEnchant
+    maven("https://repo.rosewooddev.io/repository/public/") //PlayerPoints
+}
+
+dependencies {
+    compileOnly("lilypad.client.connect:api:0.0.1-SNAPSHOT") //LilyPad
+    compileOnly("com.bencodez:votingplugin:6.13") //VotingPlugin
+    compileOnly("com.github.MilkBowl:VaultAPI:1.7.1") //Vault
+    compileOnly("me.clip:placeholderapi:2.11.3") //PlaceholderAPI
+    compileOnly("net.md-5:bungeecord-api:1.19-R0.1-SNAPSHOT") //Bungeecord
+    compileOnly("io.papermc.paper:paper-api:1.20-R0.1-SNAPSHOT") //Paper
+    compileOnly("org.black_ixx:playerpoints:3.2.5") //PlayerPoints
+    compileOnly("com.vk2gpz.tokenenchant:TokenEnchantAPI:18.37.1") //TokenEnchant
+    compileOnly("com.github.JustEli:Coins:1.10.3") //TODO: new version
+    compileOnly("com.yapzhenyie.GadgetsMenu:GadgetsMenu:4.3.32") //GadgetsMenu //TODO: new version
+    compileOnly("de.dustplanet.silkspawners.SilkSpawners:SilkSpawners:5.0.2") //SilkSpawners //TODO: new version
+    compileOnly("com.meowj:LangUtils:1.6.1") //LangUtils //TODO: new version
+    compileOnly("com.github.Realizedd:TokenManager:3.2.4") {
+        isTransitive = false
+    } //TODO: new version
+    compileOnly("com.github.Zrips:Jobs:v4.17.2") {
+        isTransitive = false
+    }
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
+
+configurations {
+    testImplementation {
+        extendsFrom(compileOnly.get())
+    }
+}
+
+tasks {
+    assemble {
+        dependsOn(shadowJar)
+    }
+
+    test {
+        useJUnitPlatform()
+        testLogging {
+            events("passed", "skipped", "failed")
+        }
+    }
+
+    compileJava {
+        options.encoding = Charsets.UTF_8.name()
+        options.compilerArgs.add("-parameters")
+    }
+
+    javadoc {
+        options.encoding = Charsets.UTF_8.name()
+    }
+
+    processResources {
+        filteringCharset = Charsets.UTF_8.name()
+    }
+
+    runServer {
+        minecraftVersion("1.20")
+    }
+
+    shadowJar {
+        archiveClassifier.set("")
+        minimize()
+//        fun reloc(pkg: String) = relocate(pkg, "org.black_ixx.relocate.$pkg")
+//        reloc("cloud.commandframework")
+//        reloc("com.github")
+//        reloc("com.google.common")
+//        reloc("com.google.inject")
+//        reloc("com.google.errorprone")
+//        reloc("com.google.j2objc")
+//        reloc("com.zaxxer")
+//        reloc("io.leangen")
+//        reloc("javax.annotation")
+//        reloc("javax.inject")
+//        reloc("org.aopalliance")
+//        reloc("org.bstats")
+//        reloc("org.checkerframework")
+//        reloc("org.jdbi")
+//        reloc("org.incendo")
+//        reloc("org.spongepowered")
+        manifest {
+            attributes(Pair("Main-Class", "org.black_ixx.bossshop.BossShop"))
+        }
+    }
+}
